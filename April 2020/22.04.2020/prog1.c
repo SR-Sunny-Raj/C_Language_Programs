2714d53bc1c51782e80c4f22ed61fef89414dc57
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-10 16:05:56 +0530

    C Programs

--- April 2020/22.04.2020/prog1.c
+++ April 2020/22.04.2020/prog1.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
- /*structure declaration*/
-struct employee{
-    char    name[30];
-    int     empId;
-    float   salary;
+/*structure declaration*/
+struct employee
+{
+    char name[30];
+    int empId;
+    float salary;
 };
- int main()
+int main()
 {
     /*declare structure variable*/
     struct employee emp;
     /*read employee details*/
     printf("\nEnter details :\n");
-    printf("Name :");          gets(emp.name);
-    printf("ID :");            scanf("%d",&emp.empId);
-    printf("Salary :");        scanf("%f",&emp.salary);
+    printf("Name :");
+    gets(emp.name);
+    printf("ID :");
+    scanf("%d", &emp.empId);
+    printf("Salary :");
+    scanf("%f", &emp.salary);
     /*print employee details*/
     printf("\nEntered details are:");
-    printf("Name: %s"   ,emp.name);
-    printf(" Id: %d"     ,emp.empId);
-    printf(" Salary: %f\n",emp.salary);
-    return 0;}
-
+    printf("Name: %s", emp.name);
+    printf(" Id: %d", emp.empId);
+    printf(" Salary: %f\n", emp.salary);
+    return 0;
+}
