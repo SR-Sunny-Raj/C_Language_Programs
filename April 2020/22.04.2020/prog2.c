c64e56f00379efa83ae77f62a0f0904d90e75c6a
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-06-15 12:09:53 +0530

    C

--- April 2020/22.04.2020/prog2.c
+++ April 2020/22.04.2020/prog2.c
@@ -1,39 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 typedef struct num
-{int a;
-int b;
-int c;
-}number;
+{
+	int a;
+	int b;
+	int c;
+} number;
 number xnum;
 int main()
 {
 	int smallest;
-printf("\n Enter the numbers: ");
-scanf("%d",&xnum.a);
-scanf("%d",&xnum.b);
-scanf("%d",&xnum.c);
+	printf("\n Enter the numbers: ");
+	scanf("%d", &xnum.a);
+	scanf("%d", &xnum.b);
+	scanf("%d", &xnum.c);
 
-if(xnum.a<xnum.b && xnum.a<xnum.c)
-{
-	smallest=xnum.a;
-	printf("\nThe smallest value is=%d",smallest);
-}
+	if (xnum.a < xnum.b && xnum.a < xnum.c)
+	{
+		smallest = xnum.a;
+		printf("\nThe smallest value is=%d", smallest);
+	}
 
-else if(xnum.b<xnum.a && xnum.b<xnum.c)
-{
-	smallest=xnum.b;
-	printf("\nThe smallest value is=%d",smallest);
+	else if (xnum.b < xnum.a && xnum.b < xnum.c)
+	{
+		smallest = xnum.b;
+		printf("\nThe smallest value is=%d", smallest);
+	}
+	else if (xnum.c < xnum.a && xnum.c < xnum.b)
+	{
+		smallest = xnum.c;
+		printf("\nThe smallest value is=%d", smallest);
+	}
+	else if (xnum.a == xnum.b && xnum.a == xnum.c)
+	{
+		smallest = xnum.a;
+		printf("\nThe smallest value is=%d", smallest);
+	}
+	return 0;
 }
-else if(xnum.c<xnum.a && xnum.c<xnum.b)
-{
-	smallest=xnum.c;
-	printf("\nThe smallest value is=%d",smallest);
-}
-else if(xnum.a==xnum.b && xnum.a==xnum.c)
-{
-  smallest=xnum.a;
-	printf("\nThe smallest value is=%d",smallest);	
-}
-return 0;
-}
-
